791a44fe05e6b4ad255f01d77103f6de84ac8cd8
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2017-08-28 21:58:19 -0500

    aadded counter to orders

---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order < ActiveRecord::Base
+	belongs_to :client, counter_cache: :count_of_clients
+	belongs_to :task, counter_cache: :count_of_tasks
+	belongs_to :comment, counter_cache: :count_of_comments
+	belongs_to :site, counter_cache: :count_of_sites
+	belongs_to :frequency, counter_cache: :count_of_frequencies
+end 
