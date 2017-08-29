add9d78a52d8a5fc97aba0cc9413273e46358433
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2017-08-29 14:30:51 -0500

    create new order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,15 @@
 class OrdersController < Sinatra::Base
  
- get '/orders/new' do
-  @tasks = Task.all
-  @frequencies = Frequency.all
-  @sites = Site.all
-  @clients = Client.all
-  erb :'orders/new'
-end
+	 get '/orders/new' do
+	  @tasks = Task.all
+	  @frequencies = Frequency.all
+	  @sites = Site.all
+	  @clients = Client.all
+	  erb :'orders/new'
+	end
 
- 
+	post '/orders' do
+		binding.pry
+	  @order = Order.create(params["orders"])
+	end
 end
